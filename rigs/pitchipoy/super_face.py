3c4ae076e4171e98df88dc20227ed7aee55ecd36
Kfir <kfir@pitchipoy.tv>
2013-03-14 14:34:43 +0200

    fixed bugs in controls and widgets code

--- rigs/pitchipoy/super_face.py
+++ rigs/pitchipoy/super_face.py
@@ -1,11 +1,11 @@
 import bpy, re
-from   mathutils     import Vector
-from   ...utils      import copy_bone, flip_bone
-from   ...utils      import strip_org, make_deformer_name, connected_children_names, make_mechanism_name
-from   ...utils      import create_circle_widget, create_sphere_widget, create_widget, create_cube_widget
-from   ...utils      import MetarigError
-from   rna_prop_ui   import rna_idprop_ui_prop_get
-from   super_widgets import create_eye_widget, create_eyes_widget, create_ear_widget, create_jaw_widget
+from   mathutils      import Vector
+from   ...utils       import copy_bone, flip_bone
+from   ...utils       import org, strip_org, make_deformer_name, connected_children_names, make_mechanism_name
+from   ...utils       import create_circle_widget, create_sphere_widget, create_widget, create_cube_widget
+from   ...utils       import MetarigError
+from   rna_prop_ui    import rna_idprop_ui_prop_get
+from   .super_widgets import create_eye_widget, create_eyes_widget, create_ear_widget, create_jaw_widget
 
 
 script = """
@@ -98,7 +98,7 @@ class Rig:
         return { 'def_bones' : def_bones }
         
     def create_tweak( self, bones ):
-
+        pass
 
     def create_ctrl( self, bones ):
         org_bones = self.org_bones
@@ -111,56 +111,62 @@ class Rig:
         eyeR_e = eb[ bones['eyes'][1] ]
         
         distance = ( eyeL_e.head - eyeR_e.head ) * 3
+        distance = distance.cross( (0, 0, 1) )
         
         eyeL_ctrl_name = strip_org( bones['eyes'][0] )
         eyeR_ctrl_name = strip_org( bones['eyes'][1] )
         
-        eyeL_ctrl_name = copy_bone( self.obj, bones['eyes'][0], eyeL_ctrl_name )
-        eyeR_ctrl_name = copy_bone( self.obj, bones['eyes'][1], eyeR_ctrl_name )
-        eyes_ctrl_name  = copy_bone( self.obj, bones['eyes'][0], 'eyes'          )
+        eyeL_ctrl_name = copy_bone( self.obj, bones['eyes'][0],  eyeL_ctrl_name )
+        eyeR_ctrl_name = copy_bone( self.obj, bones['eyes'][1],  eyeR_ctrl_name )
+        eyes_ctrl_name = copy_bone( self.obj, bones['eyes'][0], 'eyes'          )
         
         eyeL_ctrl_e = eb[ eyeL_ctrl_name ]
         eyeR_ctrl_e = eb[ eyeR_ctrl_name ]
         eyes_ctrl_e = eb[ 'eyes' ]
         
-        eyeL_ctrl_e.head[:] += distance
-        eyeR_ctrl_e.head[:] += distance
+        eyeL_ctrl_e.head    += distance
+        eyeR_ctrl_e.head    += distance
         eyes_ctrl_e.head[:] =  ( eyeL_ctrl_e.head + eyeR_ctrl_e.head ) / 2
         
         for bone in [ eyeL_ctrl_e, eyeR_ctrl_e, eyes_ctrl_e ]:
-            bone.tail[:] = bone.head + Vector( 0, 0, 0.03 )
-        
-        # Assign each eye widgets
-        self.create_eye_widget( self.obj, eyeL_ctrl_name )
-        self.create_eye_widget( self.obj, eyeR_ctrl_name )
-        
-        # Assign eyes widgets
-        self.create_eyes_widget( self.obj, eyes_ctrl_name )
+            bone.tail[:] = bone.head + Vector( [ 0, 0, 0.03 ] )
         
         ## ears controls
         earL_name = strip_org( bones['ears'][0] )
         earR_name = strip_org( bones['ears'][1] )
         
-        earL_ctrl_name = copy_bone( self.obj, bone['ears'][0], earL_name )
-        earR_ctrl_name = copy_bone( self.obj, bone['ears'][1], earR_name )
-        
-        # Assign ears widget
-        create_ear_widget( self.obj, earL_ctrl_name )
-        create_ear_widget( self.obj, earR_ctrl_name )
-        
+        earL_ctrl_name = copy_bone( self.obj, org( bones['ears'][0] ), earL_name )
+        earR_ctrl_name = copy_bone( self.obj, org( bones['ears'][1] ), earR_name )
+
         ## jaw controls
+        jaw_ctrl_name = strip_org( bones['jaw'][2] )
+        jaw_ctrl_name = copy_bone( self.obj, bones['jaw'][2], jaw_ctrl_name )
+
         jawL_org_e = eb[ bones['jaw'][0] ]
         jawR_org_e = eb[ bones['jaw'][1] ]
         jaw_org_e  = eb[ bones['jaw'][2] ]
 
-        jaw_ctrl_name = strip_org( bones['jaw'][2] )
-        jaw_ctrl_name = copy_bone( self.obj, bones['jaw'][2], jaw_ctrl_name )
+        eb[ jaw_ctrl_name ].head[:] = ( jawL_org_e.head + jawR_org_e.head ) / 2
+
+        bpy.ops.object.mode_set(mode ='OBJECT')
+
+        ## Assign widgets
+
+        # Assign each eye widgets
+        create_eye_widget( self.obj, eyeL_ctrl_name )
+        create_eye_widget( self.obj, eyeR_ctrl_name )
         
-        eb[ jaw_ctrl_name ].head[:] = ( jawL_org_e + jawR_org_e ) / 2
-        
+        # Assign eyes widgets
+        create_eyes_widget( self.obj, eyes_ctrl_name )
+
+        # Assign ears widget
+        create_ear_widget( self.obj, earL_ctrl_name )
+        create_ear_widget( self.obj, earR_ctrl_name )
+
         # Assign jaw widget
         create_jaw_widget( self.obj, jaw_ctrl_name )
-    
+
+
     def all_controls( self ):
         org_bones = self.org_bones
 
@@ -175,10 +181,10 @@ class Rig:
         org_tongue_bones  = sorted([ bone for bone in org_bones if 'tongue' in bone ])
 
         org_to_ctrls = {
-            'eyes'   : [ 'eye.L',   'eye.R'        ],
-            'ears'   : [ 'ear.L',   'ear.R'        ],
-            'jaw'    : [ 'jaw.L',   'jaw.R', 'jaw' ],
-            'teeth'  : [ 'teeth.T', 'teeth.B'      ],
+            'eyes'   : [ org('eye.L'),   org('eye.R')             ],
+            'ears'   : [ org('ear.L'),   org('ear.R')             ],
+            'jaw'    : [ org('jaw.L'),   org('jaw.R'), org('jaw') ],
+            'teeth'  : [ org('teeth.T'), org('teeth.B')           ],
             'tongue' : org_tongue_bones[-1]
         }
         
@@ -186,7 +192,8 @@ class Rig:
         self.create_ctrl(org_to_ctrls)
 
     def create_mch( self ):
-
+        pass
+        
     def create_bones(self):
         org_bones = self.org_bones
         bpy.ops.object.mode_set(mode ='EDIT')
