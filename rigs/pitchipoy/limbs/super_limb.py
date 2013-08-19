6108f5b6533dcb62a6c4cc14ce20848bb5e20e5f
PitchiPoy <tamir@pitchipoy.tv>
2013-08-19 14:54:00 +0300

    added tweaks

--- rigs/pitchipoy/limbs/super_limb.py
+++ rigs/pitchipoy/limbs/super_limb.py
@@ -46,11 +46,140 @@ class Limb:
 
         self.segments = params.segments
 
-        # Assign values to tweak layers props if opted by user
+        # Assign values to tweak/FK layers props if opted by user
         if params.tweak_extra_layers:
             self.tweak_layers = list(params.tweak_layers)
         else:
             self.tweak_layers = None
+
+        if params.fk_extra_layers:
+            self.fk_layers = list(params.fk_layers)
+        else:
+            self.fk_layers = None
+
+    def create_tweak( self ):
+        org_bones = self.org_bones
+        
+        bpy.ops.object.mode_set(mode ='EDIT')
+        eb = self.obj.data.edit_bones
+
+        tweaks         = {}        
+        tweaks['ctrl'] = []
+        tweaks['mch' ] = []
+
+        # Create and parent mch and ctrl tweaks
+        for i,org in enumerate(org_bones):
+            if i < len(org_bones) - 1:
+                # Create segments if specified
+                for j in range( self.segments ):
+                    # MCH
+                    mch = copy_bone(
+                        self.obj,
+                        org,
+                        make_mechanism_name( strip_org(org) )
+                    )
+                    
+                    eb[ mch ].length /= self.segments * 4
+
+                    # CTRL
+                    ctrl = copy_bone(
+                        self.obj,
+                        org,
+                        strip_org(org)
+                    )
+                    
+                    eb[ ctrl ].length /= self.segments * 2
+
+                    # If we have more than one segments, place the head of the 
+                    # 2nd and onwards at the correct position
+                    if j > 0:
+                        put_bone(
+                            self.obj, 
+                            mch,
+                            eb[org].head + i * eb[org].length / self.segments
+                        )
+
+                        put_bone(
+                            self.obj, 
+                            ctrl,
+                            eb[org].head + i * eb[org].length / self.segments
+                        )
+
+                    tweaks['ctrl'] += [ ctrl ]
+                    tweaks['mch' ] += [ mch  ]
+
+                    # Parenting the tweak ctrls to mchs
+                    eb[ mch  ].parent = eb[ org ]
+                    eb[ ctrl ].parent = eb[ mch ]
+
+            else: # Last limb bone - is not subdivided        
+                mch = make_mechanism_name( strip_org(org) )
+                mch = copy_bone( self.obj, org_bones[i-1], mch )
+                eb[ mch ].length = eb[org].length / 4
+                put_bone(
+                    self.obj, 
+                    mch,
+                    eb[org_bones[i-1]].tail
+                )                        
+ 
+                ctrl = strip_org(org)
+                ctrl = copy_bone( self.obj, org, ctrl )
+                eb[ ctrl ].length = eb[org].length / 2 
+
+                tweaks['mch']  += [ mch  ]
+                tweaks['ctrl'] += [ ctrl ]
+        
+                # Parenting the tweak ctrls to mchs
+                eb[ mch  ].parent = eb[ org ]
+                eb[ ctrl ].parent = eb[ mch ]
+
+        # Contraints
+        for i,b in enumerate( tweaks['mch'] ):
+            first  = 0
+            middle = trunc( len( tweaks['mch'] ) / 2 )
+            last   = len( tweaks['mch'] ) - 1
+
+            if i == first or i == middle or i == last:
+                make_constraint( self, b, {
+                    'constraint'  : 'COPY_SCALE',
+                    'subtarget'   : self.obj.pose.bones[0] # root
+                })
+            else:
+                targets = []
+                if i < trunc( len( tweaks['mch'] ) / 2 ):
+                    targets = [first,middle]
+                else:
+                    targets = [middle,last]
+
+                # Use copy transforms constraints to position each bone
+                # exactly in the location respective to its index (between
+                # the two edges)
+                make_constraint( self, b, {
+                    'constraint'  : 'COPY_TRANSFORMS',
+                    'subtarget'   : tweaks['ctrl'][targets[0]]
+                })
+                make_constraint( self, b, {
+                    'constraint'  : 'COPY_TRANSFORMS',
+                    'subtarget'   : tweaks['ctrl'][targets[1]],
+                    'influence'   : i / self.segments
+                })
+                make_constraint( self, d, {
+                    'constraint'  : 'DAMPED_TRACK',
+                    'subtarget'   : tweaks['ctrl'][i+1],
+                })                
+
+        # Ctrl bones Locks and Widgets
+        pb = self.obj.pose.bones
+        for t in tweaks['ctrl']:
+            pb[t].lock_rotation = True, False, True
+            pb[t].lock_scale    = False, True, False
+
+            create_sphere_widget(self.obj, t, bone_transform_name=None)
+            
+            if self.tweak_layers:
+                pb[t].bone.layers = self.tweak_layers 
+        
+        return tweaks
 
 
     def create_def( self, tweaks ):
@@ -90,23 +219,23 @@ class Limb:
                 eb[b].use_connect = True
 
         # Constraint def to tweaks
-        for d,t in zip(def_bones, tweaks):
-            tidx = tweaks.index(t)
+        for d,t in zip(def_bones, tweaks['ctrl']):
+            tidx = tweaks['ctrl'].index(t)
 
             make_constraint( self, d, {
                 'constraint'  : 'COPY_TRANSFORMS',
                 'subtarget'   : t
             })
 
-            if tidx != len(tweaks) - 1:
+            if tidx != len(tweaks['ctrl']) - 1:
                 make_constraint( self, d, {
                     'constraint'  : 'DAMPED_TRACK',
-                    'subtarget'   : tweaks[ tidx + 1 ],
+                    'subtarget'   : tweaks['ctrl'][ tidx + 1 ],
                 })
 
                 make_constraint( self, d, {
                     'constraint'  : 'STRETCH_TO',
-                    'subtarget'   : tweaks[ tidx + 1 ],
+                    'subtarget'   : tweaks['ctrl'][ tidx + 1 ],
                 })
 
         # Create bbone segments
@@ -118,11 +247,11 @@ class Limb:
 
         # Rubber hose drivers
         pb = self.obj.pose.bones
-        for i,t in enumerate( tweaks[1:-1] ):
+        for i,t in enumerate( tweaks['ctrl'][1:-1] ):
             # Create custom property on tweak bone to control rubber hose
             name                = 'rubber_' + t 
 
-            if i == trunc( len( tweaks[1:-1] / 2 ) ):
+            if i == trunc( len( tweaks['ctrl'][1:-1] / 2 ) ):
                 pb[t][prop] = 0.0
             else:
                 pb[t][prop] = 1.0
@@ -168,10 +297,7 @@ class Limb:
                  'mch_str'    : mch_str
         }
     
-    def create_tweak( self ):
-        pass
 
-        return { 'ctrl' : ctrls, 'mch' : mchs }
         
 
     def generate( self ):
@@ -218,6 +344,19 @@ def add_parameters( params ):
         description = "Layers for the tweak controls to be on",
         default     = tuple( [ i == 1 for i in range(0, 32) ] )
         )
+        
+    # Setting up extra layers for the FK and tweak
+    params.fk_extra_layers = bpy.props.BoolProperty( 
+        name        = "fk_extra_layers", 
+        default     = True, 
+        description = ""
+        )
+
+    params.fk_layers = bpy.props.BoolVectorProperty(
+        size        = 32,
+        description = "Layers for the FK controls to be on",
+        default     = tuple( [ i == 1 for i in range(0, 32) ] )
+        )
 
 
 def parameters_ui(layout, params):
@@ -232,28 +371,29 @@ def parameters_ui(layout, params):
     r = layout.row()
     r.prop(params, "tail_pos")
 
-    r = layout.row()
-    r.prop(params, "tweak_extra_layers")
-    r.active = params.tweak_extra_layers
-    
-    col = r.column(align=True)
-    row = col.row(align=True)
+    for layer in [ 'tweak', 'fk' ]:
+        r = layout.row()
+        r.prop(params, layer + "_extra_layers")
+        r.active = params.tweak_extra_layers
+        
+        col = r.column(align=True)
+        row = col.row(align=True)
 
-    for i in range(8):
-        row.prop(params, "tweak_layers", index=i, toggle=True, text="")
+        for i in range(8):
+            row.prop(params, layer + "_layers", index=i, toggle=True, text="")
 
-    row = col.row(align=True)
+        row = col.row(align=True)
 
-    for i in range(16,24):
-        row.prop(params, "tweak_layers", index=i, toggle=True, text="")
+        for i in range(16,24):
+            row.prop(params, layer + "_layers", index=i, toggle=True, text="")
 
-    col = r.column(align=True)
-    row = col.row(align=True)
+        col = r.column(align=True)
+        row = col.row(align=True)
 
-    for i in range(8,16):
-        row.prop(params, "tweak_layers", index=i, toggle=True, text="")
+        for i in range(8,16):
+            row.prop(params, layer + "_layers", index=i, toggle=True, text="")
 
-    row = col.row(align=True)
+        row = col.row(align=True)
 
-    for i in range(24,32):
-        row.prop(params, "tweak_layers", index=i, toggle=True, text="")
+        for i in range(24,32):
+            row.prop(params, layer + "_layers", index=i, toggle=True, text="")
